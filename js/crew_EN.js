0eab7dd3fe41aff5351f77a6d605603fcad2e7de
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-10-05 14:21:59 -0600

    Error message added

--- js/crew_EN.js
+++ js/crew_EN.js
@@ -82,7 +82,7 @@ var crewSingle = (function ($) {
 
         // Triggered if the JSON fails
         function jsonError() {
-            $('#crewBio').html('Lo sentimos pero la página no cargó correctamente, inténtelo de nuevo más tarde');    
+            $('#crewBio').html("Sorry, the page didn't load properly, try again later");    
         }
     };
 
